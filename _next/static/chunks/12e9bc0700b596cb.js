5081d824171a8039caac186ab81cc259f9094334
Moosa <moosa.binnaseem09@gmail.com>
2025-11-02 19:48:59 +0500

    Updates

---
+++ _next/static/chunks/12e9bc0700b596cb.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/fb9e0659ae82cb30.js",
+  "static/chunks/be97cf4c9943af33.js",
+  "static/chunks/turbopack-a257aae1f1ff0c62.js"
+])
---
+++ _next/static/chunks/527aa0e949eeb963.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/719fafde105112a3.js",
+  "static/chunks/be97cf4c9943af33.js",
+  "static/chunks/turbopack-0dae2a511ff11960.js"
+])
